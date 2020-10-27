94c66e9b7c53bfe1533838fbc783f628c8d876ab
salis <salisshuaibu11@gmail.com>
2020-10-27 01:09:07 +0000

    Created my music playlist layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 import '../scss/app.scss';
+import * as searchView from '../js/views/search';
 
 // Your JS Code goes here
+searchView.multiply(3, 3);
---
+++ src/js/views/search.js
@@ -0,0 +1,4 @@
+export const multiply = (x, y) => {
+  const sum = x * y;
+  console.log(sum);
+}
